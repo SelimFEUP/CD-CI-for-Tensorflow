6d278b5a4927fc9022dfd49ba948cf2cbb547601
SelimFEUP <84468279+SelimFEUP@users.noreply.github.com>
2025-06-09 17:30:49 +0100

    Update model.py

--- src/model.py
+++ src/model.py
@@ -2,10 +2,20 @@ import tensorflow as tf
 
 def build_model(input_shape, output_shape):
     model = tf.keras.Sequential([
-        tf.keras.layers.Input(shape=input_shape),
+        tf.keras.layers.Input(shape=input_shape),  # (input_steps, num_features)
+
+        tf.keras.layers.LSTM(128, return_sequences=True),
+        tf.keras.layers.Dropout(0.3),
         tf.keras.layers.LSTM(64, return_sequences=False),
+        tf.keras.layers.Dropout(0.3),
+
+        tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.Dense(output_shape[0] * output_shape[1]),
-        tf.keras.layers.Reshape(output_shape)
+
+        tf.keras.layers.Reshape(output_shape)  # (output_steps, num_features)
     ])
-    model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+                  loss='mse',
+                  metrics=['mae'])
     return model
